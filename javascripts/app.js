cb68c26d843512b6057ffe07cf1475c750e87c97
2d1zzy <168884170+2d1zzy@users.noreply.github.com>
2024-09-07 22:04:03 +0700

    Re-wrote the js, need to continue with refactoring to loop everything.

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,6 +1,31 @@
 var main = function () {
     "use strict";
 
+    var makeTabActive = function (tabNumber) {
+        // construct the selector from the tabNumber
+        var tabSelector = ".tabs a:nth-child(" + tabNumber + ") span";
+            $(".tabs span").removeClass("active");
+            $(tabSelector).addClass("active");
+            $("main .content").empty();
+        };
+
+//Continue with Refactoring the code, by using a loop
+
+        $(".tabs a:nth-child(1)").on("click", function () {
+            makeTabActive(1);
+            return false;
+        });
+        
+        $(".tabs a:nth-child(2)").on("click", function () {
+            makeTabActive(2);
+            return false;
+        });
+
+        $(".tabs a:nth-child(3)").on("click", function () {
+            makeTabActive(3);
+            return false;
+        });
+    };
 
     console.log("hello world!");
 };
