af1fd6f47f59d442c096423d6b5075049a238bec
2d1zzy <168884170+2d1zzy@users.noreply.github.com>
2024-09-08 09:12:35 +0700

    Implemented tab-click handler with jquery and refactor forEach loop.

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,16 +1,38 @@
-const main = function () {
+function main() {
     "use strict";
 
-    const tabs = $(".tabs span");
+    const tabs = $(".tabs a span");
 
     tabs.toArray().forEach(function (element) {
         // create a click handler for this element
-        $(element).on("click", function () { 
-            tabs.removeClass("active"); // use the cached variable
+    
+        // create a click handler for this element
+        
+        // create a click handler for this element
+        $(element).on("click", function () {
+            // since we're using the jquery version of element,
+            // we'll go ahead and create a temporary variable
+            // so we don't need to keep recreating it
+            const $element = $(element); // Use const because $element isn't reassigned
+            tabs.removeClass("active");
+            $element.addClass("active");
             $("main .content").empty();
+
+            // Check which tab was clicked
+            if ($element.parent().is(":nth-child(1)")) {
+                console.log("FIRST TAB CLICKED!");
+            } else if ($element.parent().is(":nth-child(2)")) {
+                console.log("SECOND TAB CLICKED!");
+            } else if ($element.parent().is(":nth-child(3)")) {
+                console.log("THIRD TAB CLICKED!");
+            }
+
             return false;
         });
     });
-};
 
-$(document).ready(main);
+}
+
+
+
+$(document).ready(main); 
